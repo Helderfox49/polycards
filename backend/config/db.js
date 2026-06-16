c75c7d13f1b5d6d9ac59c148d5040a36c4a177d0
Helderfox49 <mbottoguyzo2012@gmail.com>
2026-06-16 14:02:08 +0100

    Fix: Fix sur le fichier db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,25 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        // Vérifie que la variable d'environnement existe
-        if (!process.env.MONGO_URI) {
-            throw new Error('MONGO_URI manquante dans les variables d’environnement');
-        }
-
-        const isProd = process.env.NODE_ENV === 'production';
-        const uri = process.env.MONGO_URI;
-
-        await mongoose.connect(uri);
-
-        console.log(
-            `Base de données connectée sur : ${
-                isProd ? 'PRODUCTION (MongoDB Atlas)' : 'LOCAL'
-            }`
-        );
-
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "polycards"
+        });
+        console.log(`MongoDB Connecté : ${conn.connection.host}`);
+        // console.log(`MongoDB connecté sur la base de données : ${conn.connection.db.namespace}`);
     } catch (error) {
-        console.error(`Erreur de connexion MongoDB : ${error.message}`);
-        process.exit(1);
+        console.error(`Erreur de connexion : ${error.message}`);
+        process.exit(1); // Arrête le serveur en cas d'échec
     }
 };
 
